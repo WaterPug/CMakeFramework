d7314cea4a73f3cf9d6067ec73892bce0820db5e
Water Pug <david.lupien@polymtl.ca>
2020-05-03 15:31:07 -0400

    Initial test commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OpenglBasicTests
+
+Template project taken from : https://github.com/lefticus/cpp_starter_project
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+
+//#include <spdlog/spdlog.h>
+
+int main(int , const char **)
+{
+  //Use the default logger (stdout, multi-threaded, colored)
+  //spdlog::info("Hello, {}!", "World");
+  // fmt::print("Hello, from {}\n", "{fmt}");
+  return 1;
+}
