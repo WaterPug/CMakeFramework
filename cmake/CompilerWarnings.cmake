be405b51a32d63e811ed890f32802946c964e803
Water Pug <david.lupien@polymtl.ca>
2020-05-03 15:43:43 -0400

    test basic appveyor

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -3,7 +3,7 @@
 # https://github.com/lefticus/cppbestpractices/blob/master/02-Use_the_Tools_Available.md
 
 function(set_project_warnings project_name)
-  option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" TRUE)
+  option(WARNINGS_AS_ERRORS "Treat compiler warnings as errors" FALSE) # TODO: Enable this
   
   # TODO: Find /w#### that could be useful
   set(MSVC_WARNINGS 
